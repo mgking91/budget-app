39d84980ff5ed4bc6eed3ed99da2d88b2bbae104
mgking91 <69322251+mgking91@users.noreply.github.com>
2020-11-10 20:56:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// Selectors
+let updateBudgetButton = document.querySelector('#update_budget');
+let addExpenseButton = document.querySelector('#add_expense');
+let monthlyBudget = document.querySelector('#monthly_budget');
+let incomeInput = document.querySelector('#income_input');
+let amountInput = document.querySelector('#amount_input');
+let remainingBalance = document.querySelector('#remaining_balance');
+let nameInput = document.querySelector('#name_input');
+let expenseList = document.querySelector('#expense_list');
+let totalExpenses = document.querySelector('#total_expenses');
+
+// events
+updateBudgetButton.onclick = updateBudget;
+addExpenseButton.onclick = addExpense;
+
+//Variables
+let monthlyIncome = 0;
+let expenses = [];
+let expenseTotal = 0;
+let balance = 0;
+
+// Functions
+function updateBudget(event) {
+    event.preventDefault();
+    monthlyIncome = incomeInput.value;
+    monthlyBudget.innerText = "$" + monthlyIncome;
+    incomeInput.value = '';
+    updateBalance();
+}
+
+function updateBalance(){
+    balance = monthlyIncome - expenseTotal;
+    remainingBalance.innerText = "$" + balance;
+    if(balance < 0) {
+        remainingBalance.classList.remove('green');                         
+        remainingBalance.classList.add('red');
+     }
+      else {
+            remainingBalance.classList.remove('red');
+            remainingBalance.classList.add('green');
+        }
+    
+}
+
+function addExpense (event){
+    event.preventDefault();
+    let expense = {
+        expenseName: nameInput.value,
+        expenseAmount: amountInput.value
+    }
+    let newExpense = document.createElement('p');
+    newExpense.innerText = expense.expenseName + ": $" + expense.expenseAmount;
+    
+    expenseList.appendChild(newExpense);
+    expenseAmount = parseInt(amountInput.value);
+    expenses.push(expenseAmount);
+    nameInput.value='';
+    amountInput.value='';
+    updateExpenseTotal();
+}
+
+
+function updateExpenseTotal() {
+    expenseTotal = 0;
+    // initial expression; condition; incrementExpression
+    for(let i = 0; i < expenses.length; i++){
+        expenseTotal = expenseTotal + expenses[i];
+        
+    }
+    totalExpenses.innerText = "$" + expenseTotal;
+    updateBalance();
+}
